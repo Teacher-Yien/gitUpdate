9e023c8a740d336c8323c22c97ed4a70035b1e97
vyvuth <vyvuth191@gmail.com>
2024-06-08 13:17:48 +0700

    ab

--- lession.js
+++ lession.js
@@ -6,5 +6,10 @@ const myName="Nana";
 //myName=1002;
 console.log(myName);
 
-let Array =  [100,200,300];
+let Array1 =  [100,200,300];
 console.log(Array);
+
+//vy
+let Array2 = [12.52, 200, 54.25, 500];
+let result = Math.max(...Array2);
+console.log(Array2);
