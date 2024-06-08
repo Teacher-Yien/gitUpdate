0d325b2b059e9a8df3a0ce0ca5f299e017614df9
SOTHYIEN <121343858+SOTHYIEN@users.noreply.github.com>
2024-06-08 13:08:31 +0700

    team up

--- lession.js
+++ lession.js
@@ -1,10 +1,16 @@
 // Data type   var , let  and const
-var Name="FaFa";
-Name=1001;
-console.log(Name);
-const myName="Nana";
-//myName=1002;
-console.log(myName);
+// Teacher yien
+// var Name="FaFa";
+// Name=1001;
+// console.log(Name);
+// const myName="Nana";
+// //myName=1002;
+// console.log(myName);
 
-let Array =  [100,200,300];
-console.log(Array);
+// 1. copy value in array
+let Array =  [10,200,40,99,2988,300];
+let result = Math.max(...Array);
+console.log(result);
+// spread operator in js (...)
+// students  chenda
+
