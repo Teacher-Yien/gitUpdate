da537087bb36a1f31ea7ff9f9f2fce19a11ef7cf
vyvuth <vyvuth191@gmail.com>
2024-06-14 23:17:00 +0700

    Vy update data

--- lession.js
+++ lession.js
@@ -8,25 +8,32 @@
 // console.log(myName);
 
 // 1. copy value in array
-let Array =  [10,200,40,99,2988,300];
-let result = Math.max(...Array);
-console.log(result);
+//let Array =  [10,200,40,99,2988,300];
+//let result = Math.max(...Array);
+//console.log(result);
 // spread operator in js (...)
 // students  chenda
 // let java
-let java = "Do you love me?";
-let Array1 =  [100,200,300];
-console.log(Array);
+//let java = "Do you love me?";
+//let Array1 =  [100,200,300];
+//console.log(Array);
 
 //vy
-let First = [12.52,200,54.25,500];
-let second = Math.max(...First);
-console.log(second);
-var myName = "hello js";    
+//let First = [12.52,200,54.25,500];
+//let second = Math.max(...First);
+//console.log(second);
+//var myName = "hello js";    
+//
+//const AB = [0,0.001,25,30,65,180];
+//const maximum = Math.max(...AB);
+//const minimum = Math.min(...AB)
+//console.log(minimum);
 
-const AB = [0,0.001,25,30,65,180];
-const maximum = Math.max(...AB);
-const minimum = Math.min(...AB)
-console.log(minimum);
+let totalScore = Math.random()*100+1;
+if (totalScore>=50){
+    console.log("Pasted Exam");
+} else if (totalScore<50){
+    console.log("Failed Exam");
+}
 
 
